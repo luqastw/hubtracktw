8f2452a6cf9bedb505cf3b7818278bfd7c49646f
luqastw <lucasborcem1@gmail.com>
2025-12-04 23:58:56 -0300

    feat: initial setup.

---
+++ src/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    REDIS_URL: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
