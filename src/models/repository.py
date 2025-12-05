11af74cd0176f9b068a5fd0c15e20bf6eca92e1d
luqastw <lucasborcem1@gmail.com>
2025-12-05 00:15:19 -0300

    feat: alembic configured.

--- src/models/repository.py
+++ src/models/repository.py
@@ -1,13 +1,14 @@
 from src.db.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.sql import func, datetime
+from sqlalchemy.sql import func
 from sqlalchemy import String, Integer
+from datetime import datetime
 
-class RepositoryBase(Base):
+class RepositoryModel(Base):
     __tablename__ = "repositories"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column()
     owner: Mapped[str] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    last_analyzed_at: Mapped[datetime | None] = mapped_column(server_default=func.now())
+    last_analyzed_at: Mapped[datetime | None] = mapped_column(nullable=True)
