553fda67d6d9ca4fd31e9c7a01211011a4c88784
luqastw <lucasborcem1@gmail.com>
2025-12-07 21:20:17 -0300

    feat: repository schema added.

---
+++ src/schemas/repository.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+class RepositoryBase(BaseModel):
+    name: str
+    owner: str
+    url: str
+
+class RepositoryCreate(RepositoryBase):
+    pass
+
+class RepositoryResponse(RepositoryBase):
+    id: int
+    create_at: datetime | None
+    last_analyzed_at: datetime | None
+
+    model_config = ConfigDict(from_attributes=True)
