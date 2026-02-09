0cc07b7af6710cce63e9c392c57a3b9bb9fc8852
luqastw <lucasborcem1@gmail.com>
2026-02-09 10:39:26 -0300

    chore: added rich

--- src/hubtracktw/cli.py
+++ src/hubtracktw/cli.py
@@ -1,8 +1,11 @@
+from rich.table import Table
+from rich.console import Console
 import typer
 
 from hubtracktw.client import GitHubClient
 
 app = typer.Typer()
+console = Console()
 
 
 @app.command()
@@ -17,3 +20,13 @@ def analyze(repo: str):
 
     data = client.get_repo(owner, name)
     print(f"{data['full_name']} {data['stargazers_count']}")
+
+    table = Table(title="hubtracktw")
+    table.add_column("Metric")
+    table.add_column("Value")
+    table.add_row("Name", data["full_name"])
+    table.add_row("Stars", str(data["stargazers_count"]))
+    table.add_row("Forks", str(data["forks_count"]))
+    table.add_row("Issues", str(data["open_issues_count"]))
+    table.add_row("Language", data["language"])
+    console.print(table)
